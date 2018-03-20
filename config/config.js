0c2f7d22ce6785f9cf05548af8a39c8da2ed00ca
spiritkiddie <spiritkiddie@github.com>
2018-03-20 07:32:12 -0700

    Images change and layouts,service routes delated

--- config/config.js
+++ config/config.js
@@ -3,6 +3,6 @@ module.exports =  {
     secretKey: 'GetWay-a4f8071f-c873-4447-8ee2', 
     SMTP_HOST_USERNAME: 'career@greenwavetelecom.com',
     SMTP_HOST_PASSWORD: 'careeremail',
-    SMTP_HOST_EMAIL1: 'career@greenwavetelecom.com',
+    SMTP_HOST_EMAIL1: 'plant@greenwayin.org',
     SMTP_HOST_EMAIL2: 'ceo@greenwavetelecom.com'
 }
--- routes/route.js
+++ routes/route.js
@@ -48,10 +48,6 @@ router.get('/about', (req, res) =>{
     res.render('about');
 });
 
-router.get('/services', (req, res) =>{
-    res.render('services');
-});
-
 router.get('/portfolio', (req, res) =>{
     res.render('portfolio');
 });
